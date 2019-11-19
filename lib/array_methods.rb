402fb21bd70c9c901424f5ff471a2dfa68195190
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-19 02:38:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,21 +1,20 @@
 def using_include(array, element)
 famous_cats = ["lil' bub", "grumpy cat", "Maru"]
 famous_cats.include?("Garfield")
-  => false
 famous_cats.include?("Maru")
-  => true
+
 end
 
 def using_sort(array)
 famous_cats = ["lil' bub", "grumpy cat", "maru"]
 famous_cats.sort
-  => ["grumpy cat", "lil' bub", "maru"]
+
 end
 
 def using_reverse(array)
 famous_wizards = ["Dumbledore", "Gandalf", "Merlin"]
 famous_wizards.reverse
-=> ["Merlin", "Gandalf", "Dumbledore"]
+
 
 end
 
