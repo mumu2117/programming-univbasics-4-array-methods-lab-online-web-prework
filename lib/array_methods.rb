9f1ee73610388c3bd48bb885d77422b1821fc2eb
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-19 02:37:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,19 @@
 def using_include(array, element)
-
+famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+famous_cats.include?("Garfield")
+  => false
+famous_cats.include?("Maru")
+  => true
 end
 
 def using_sort(array)
-
+famous_cats = ["lil' bub", "grumpy cat", "maru"]
+famous_cats.sort
+  => ["grumpy cat", "lil' bub", "maru"]
 end
 
 def using_reverse(array)
-
+a
 end
 
 def using_first(array)
